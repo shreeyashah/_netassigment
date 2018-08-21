fa6dbe6bf25fa3fd18d2ce909120d46b036ae745
shreeya <shreeya.shah.ss@gmail.com>
2018-08-21 09:55:30 +0530

    third commit

---
+++ Identity_user/Identity_user/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Identity_user.MvcApplication" Language="C#" %>
---
+++ Identity_user/Identity_user/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity_user.Startup))]
+namespace Identity_user
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
